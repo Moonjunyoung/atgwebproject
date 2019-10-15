e3c7b34aaf9bf4e58d5696ed871ba47fbe45af33
Moonjunyoung <kt_sdsd@naver.com>
2019-10-15 10:57:48 +0900

    ComponentNameList view add
    add Component

--- src/Components/ComponentNameListView.js
+++ src/Components/ComponentNameListView.js
@@ -1,7 +1,33 @@
 import React, { useState } from 'react'
+import Select from 'react-select'
+import '../css/ComponentNameListView.css'
+
+const customStyles = {
+  option: (provided, state) => ({
+    ...provided,
+    borderBottom: '1px dotted pink',
+    color: state.isSelected ? 'red' : 'blue',
+    padding: 20
+  })
+}
 
 const ComponentNameListView = () => {
-  return <>componentName</>
+  const handleSelect = newvalue => {
+    console.log(newvalue)
+  }
+
+  return (
+    <div>
+      <Select
+        onChange={handleSelect}
+        isMulti
+        name='colors'
+        styles={customStyles}
+        className='basic-multi-select'
+        classNamePrefix='select'
+      />
+    </div>
+  )
 }
 
 export default ComponentNameListView
--- src/Container/MainContainer.js
+++ src/Container/MainContainer.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import ComponentNameListView from '../Components/ComponentNameListView'
+import styled, { css } from 'styled-components'
+
+const Title = styled.h1`
+  text-align: center;
+  color: grey;
+`
 
 const MainContainer = () => {
   return (
     <>
+      <Title>PMBD Data View</Title>
       <ComponentNameListView />
     </>
   )
